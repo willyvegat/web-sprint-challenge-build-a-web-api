ad27656b751294c550f3aa18255a3c7c0f70c806
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2023-07-06 21:44:55 -0400

    testing of the checkProjectId middleware works plus endpoint to get projects

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,19 +1,23 @@
 const Projects = require('./projects-model');
 
 async function checkProjectsId(req, res, next) {
-    const { id } = req.params;
+    // const { id } = req.params;
     try {
-        const project = await Projects.get(id);
-        if (project) {
+        const project = await Projects.get(req.params.id);
+        if (!project) {
+            next({ status: 404, message: `No project with the given id ${req.params.id}`})
+        } else {
             req.project = project;
             next();
-        } else {
-            next({ status: 404, message: `No project with the given id ${id}`})
         }
     } catch (err) {
         next(err);
     }
 }
+
+// function checkProject (req, res, next) {
+
+// }
 
 module.exports = {
     checkProjectsId,
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,20 +1,44 @@
-// Write your "projects" router here!
 const express = require('express');
 const { checkProjectsId } = require('./projects-middleware');
-const Projects = require('./projects-model');
+const Project = require('./projects-model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            res.json(projects);
+        })
+        .catch(next);
+  
+});
+
 router.get('/:id', checkProjectsId, (req, res) => {
-    // console.log(req.project);
-    res.json(req.project);
-})
+    console.log(req.project);
+    // res.json(req.project);
+});
+
+router.post('/', (req, res) => {
+   
+});
+
+router.put('/:id', checkProjectsId, (req, res) => {
+   
+});
+
+router.delete('/:id', checkProjectsId, (req, res) => {
+   
+});
+
+router.get('/:id/actions', checkProjectsId, (req, res) => {
+   
+});
 
 router.use((error, req, res, next) => { // eslint-disable-line
     res.status(error.status || 500).json({
         message: error.message,
         customMessage: "Router is not working!"
     })
-})
+});
 
 module.exports = router;
